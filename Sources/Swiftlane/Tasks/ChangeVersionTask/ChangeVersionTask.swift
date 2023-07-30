0bb44cd6a664749fd50953b5de03a0614c93ea82
Vyacheslav Zhivetyev <slavyan6363@gmail.com>
2023-07-31 00:54:39 +0200

    removed unnecessary imports of GitLabAPI

--- Sources/Swiftlane/Tasks/ChangeVersionTask/ChangeVersionTask.swift
+++ Sources/Swiftlane/Tasks/ChangeVersionTask/ChangeVersionTask.swift
@@ -1,7 +1,6 @@
 
 import Foundation
 import Git
-import GitLabAPI
 import Guardian
 import Networking
 import SwiftlaneCore
--- Sources/Swiftlane/Tasks/GuardianCheckAuthorTask/GuardianCheckAuthorTask.swift
+++ Sources/Swiftlane/Tasks/GuardianCheckAuthorTask/GuardianCheckAuthorTask.swift
@@ -2,7 +2,6 @@
 
 import Foundation
 import Git
-import GitLabAPI
 import Guardian
 import SwiftlaneCore
 
